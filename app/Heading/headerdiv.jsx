438a95d5d6c47928d5dd20e023bfe65a4ac6eeab
JaxCavalera <cxstudios@hotmail.com>
2015-11-13 13:15:42 +1000

    problem with old class type used for new es6class

--- app/Heading/headerdiv.jsx
+++ app/Heading/headerdiv.jsx
@@ -9,66 +9,43 @@ import './heading.css';
 
 export default class HeaderDiv extends React.Component
 {
-//	Functions and code for making dom elements inside render do stuff goes here
-//	register button click function
-	regBtn(e)
-	{
-		let uname = $('#username').val();
-		switch (true)
-		{
-//	case = true if uname is empty or contains 1 or more orphan spaces
-			case (uname === '' || uname === 'Enter Valid Username' || /^[.?\s+.?]/.test(uname)) :
-				$('#username').val('Enter Valid Username');
-				$('#username').addClass("inputError");
-				break;
+//    Functions and code for making dom elements inside render do stuff goes here
 
-			default:$.ajax({
-					url: 'LOCAL_IP:8081',
-					dataType: 'json',
-					type: 'POST',
-					data: uname,
-					success: function(data)
-					{
-						console.log(data+'Has been registered');
-					}
-			});
-		}
-	}
+    getInitialState()
+    {
+        return
+        {
+            username: ''
+            password: ''
+        };
+    }
 
-//	clear inputError class css styling on focus
-		unameFocus(e)
-		{
-			switch (true) {
-				case (e.target.className !== "logFormInput") :
-					e.target.className = "logFormInput";
-					e.target.value = '';
-				break;
-				default:
-				return false;
-			}
-		}
+    regBtn(e)
+    {
+        console.log(e);
+    }
 
-	render()
-	{
-		return (
-			<div className="headBar">
-				<div className="logoPanel">
-					<img className="tagLogo" src={taglogo} alt="Tagged Logo"/>
-				</div>
-				<div className="loginPanel">
-					<form className="authForm">
-						<span className="formTitle">My Account</span>
-						<input className="logFormInput" type="text" id="username" placeholder="Username" onFocus={this.unameFocus.bind(this)}/>
-						<input className="logFormInput" type="password" id="password" placeholder="Password"/>
-						<div className="formBtns">
-							<button className="logFormInput" type="button" id="register" onClick={this.regBtn.bind(this)}>Register</button>
-							<button className="logFormInput" type="button" id="login">Login</button>
-						</div>
-					</form>
-				</div>
-			</div>
-		);
-	}
+    render()
+    {
+        return (
+            <div className="headBar">
+                <div className="logoPanel">
+                    <img className="tagLogo" src={taglogo} alt="Tagged Logo"/>
+                </div>
+                <div className="loginPanel">
+                    <form className="authForm">
+                        <span className="formTitle">My Account</span>
+                        <input className="logFormInput" type="text" id="username" placeholder="Username"/>
+                        <input className="logFormInput" type="password" id="password" placeholder="Password"/>
+                        <div className="formBtns">
+                            <button className="logFormInput" type="button" id="register" onClick={this.regBtn.bind(this)}>Register</button>
+                            <button className="logFormInput" type="button" id="login">Login</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        );
+    }
 }
 
 HeaderDiv.displayName = 'HeaderDiv';
