6c303a3abefe155410de17216fc40ba1b7bddba7
JaxCavalera <cxstudios@hotmail.com>
2015-11-15 12:26:09 +1000

    weird stuff happening here

--- app/Heading/headerdiv.jsx
+++ app/Heading/headerdiv.jsx
@@ -19,13 +19,37 @@ export default class HeaderDiv extends React.Component
         this.regBtn = this.regBtn.bind(this);
         this.uname = this.uname.bind(this);
         this.pword = this.pword.bind(this);
+        this.unameFocus = this.unameFocus.bind(this);
+        this.unameBlur = this.unameBlur.bind(this);
 
 //      Set the initial state values for changable DOM elements
         this.state = {
             unameRaw: "Username",
             pwordRaw: "",
-            errorTxt: ["Change Username", "Password Is Missing"]
-        };
+            errorTxt: ["Create New Account or Login", "Password Is Missing"]
+        }
+    }
+
+//  If the username input gets clicked on and = "username" reset to blank
+    unameFocus(e)
+    {
+        if (this.state.unameRaw === "Username")
+        {
+            this.setState({unameRaw: ""});
+
+            let error1pos = this.state.errorTxt.indexOf("Create New Account or Login");
+            if (error1pos !== -1)
+            {
+                this.state.errorTxt.splice(error1pos, 1);
+            }
+        }
+        console.log(this.state.unameRaw);
+    }
+
+//  If the username input loses focus confirm contents
+    unameBlur(e)
+    {
+        console.log(this.state.unameRaw);
     }
 
     uname(e)
@@ -34,50 +58,57 @@ export default class HeaderDiv extends React.Component
         let usertxt = e.target.value;
 
 //      Validate the Username Field
-        switch (true) {
-            case (usertxt === "") :
-                if (this.state.errorTxt.indexOf("Username Is Missing") === -1)
-                {
-                    this.state.errorTxt.push("Username Is Missing");
-                };
-                break;
-            case (usertxt === "Username") :
-                if (this.state.errorTxt.indexOf("Change Username") === -1)
-                {
-                    this.state.errorTxt.push("Change Username");
-                };
-                break;
-            case (/\s+/.test(usertxt)) :
-                if (this.state.errorTxt.indexOf("Username Has Spaces") === -1)
-                {
-                    this.state.errorTxt.push("Username Has Spaces");
-                };
-                break;
-            default:
-//     Check if Error 1 exists and if it does, remove it from the errorTxt array
-                let error1pos = this.state.errorTxt.indexOf("Username Is Missing");
-                if (error1pos !== -1)
-                {
-                    this.state.errorTxt.splice(error1pos, 1);
-                };
+        if (usertxt.length === 0)
+        {
+            if (this.state.errorTxt.indexOf("Username Is Missing") === -1)
+            {
+                this.state.errorTxt.push("Username Is Missing");
+            }
+        }
+        else
+        {
+//      Check if Error 1 exists and if it does, remove it from the errorTxt array
+            let error1pos = this.state.errorTxt.indexOf("Username Is Missing");
+            if (error1pos !== -1)
+            {
+                this.state.errorTxt.splice(error1pos, 1);
+            }
+        }
 
-//     Check if Error 2 exists and if it does, remove it from the errorTxt array
-                let error2pos = this.state.errorTxt.indexOf("Change Username");
-                if (error2pos !== -1)
-                {
-                    this.state.errorTxt.splice(error2pos, 1);
-                };
+        if (usertxt === "Username")
+        {
+            if (this.state.errorTxt.indexOf("Change Username") === -1)
+            {
+                this.state.errorTxt.push("Change Username");
+            }
+        }
+        else
+        {
+//      Check if Error 2 exists and if it does, remove it from the errorTxt array
+            let error2pos = this.state.errorTxt.indexOf("Change Username");
+            if (error2pos !== -1)
+            {
+                this.state.errorTxt.splice(error2pos, 1);
+            }
+        }
 
-//     Check if Error 3 exists and if it does, remove it from the errorTxt array
-                let error3pos = this.state.errorTxt.indexOf("Username Has Spaces");
-                if (error3pos !== -1)
-                {
-                    this.state.errorTxt.splice(error3pos, 1);
-                };
-
-                console.log("Username is Valid");
-        };
-    }
+        if (/\s+/g.test(usertxt))
+        {
+            if (this.state.errorTxt.indexOf("Username Has Spaces") === -1)
+            {
+                this.state.errorTxt.push("Username Has Spaces");
+            }
+        }
+        else
+        {
+//      Check if Error 3 exists and if it does, remove it from the errorTxt array
+            let error3pos = this.state.errorTxt.indexOf("Username Has Spaces");
+            if (error3pos !== -1)
+            {
+                this.state.errorTxt.splice(error3pos, 1);
+            }
+        }
+   }
 
     pword(e)
     {
@@ -138,7 +169,7 @@ export default class HeaderDiv extends React.Component
                 <div className="loginPanel">
                     <form className="authForm">
                         <span className="formTitle">My Account</span>
-                        <input className="logFormInput" maxLength="20" type="text" value={this.state.unameRaw} onChange={this.uname}/>
+                        <input className="logFormInput" maxLength="20" type="text" value={this.state.unameRaw} onChange={this.uname} onFocus={this.unameFocus} onBlur={this.unameBlur}/>
                         <input className="logFormInput" maxLength="20" type="password" value={this.state.pwordRaw} onChange={this.pword}/>
                         <div className="formBtns">
                             <button className="logFormInput" type="button" onClick={this.regBtn}>Register</button>
