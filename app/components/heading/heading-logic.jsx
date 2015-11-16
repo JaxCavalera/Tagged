98b202f7c5dfde4c4d5e116ecca2737ca243e7a4
JaxCavalera <cxstudios@hotmail.com>
2015-11-16 17:27:31 +1000

    attempting to get my first redux button click state processed

--- app/components/heading/heading-logic.jsx
+++ app/components/heading/heading-logic.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import {connect} from 'react-redux';
+//  connect() gives the component access to the state tree (a.k.a store)
+
 
 //  jquery and ip modules for AJAX usage only
 import $ from 'jquery';
