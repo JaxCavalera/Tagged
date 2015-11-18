e947ae1ab30078ca1ab4eca20a17f279afe2c7cb
JaxCavalera <cxstudios@hotmail.com>
2015-11-18 14:59:44 +1000

    updates

--- app/components/heading/heading-reducer.jsx
+++ app/components/heading/heading-reducer.jsx
@@ -1,18 +1,18 @@
 let initialState = {};
 
-let regBtnReducer = (currentState = {}, action) => {
-    switch (action.type) {
-        case 'REG_BTN_CLICK':
-            return {
-                ...currentState,
-                message: action.message,
-            };
-            break;
-        default:
-            return currentState;
-    }
-};
-
 export default function reducer(state = initialState, action) {
+    let regBtnReducer = (currentState = {}, action) => {
+        switch (action.type) {
+            case 'REG_BTN_CLICK':
+                return {
+                    ...currentState,
+                    message: action.message,
+                };
+                break;
+            default:
+                return currentState;
+        }
+    };
+
     return state;
 }
