4ff5a349b71db812cf913cfa88bee032f6b177ce
JaxCavalera <cxstudios@hotmail.com>
2015-11-18 15:04:47 +1000

    working reducer finally

--- app/components/heading/heading-reducer.jsx
+++ app/components/heading/heading-reducer.jsx
@@ -1,18 +1,16 @@
 let initialState = {};
 
-export default function reducer(state = initialState, action) {
-    let regBtnReducer = (currentState = {}, action) => {
-        switch (action.type) {
-            case 'REG_BTN_CLICK':
-                return {
+let regBtnReducer = (currentState = {}, action) => {
+    switch (action.type) {
+        case 'REG_BTN_CLICK':
+            return {
                     ...currentState,
                     message: action.message,
                 };
-                break;
-            default:
-                return currentState;
-        }
-    };
+            break;
+        default:
+            return currentState;
+    }
+};
 
-    return state;
-}
+export default regBtnReducer;
