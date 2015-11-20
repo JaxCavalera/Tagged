d4dc25ea1f2f0b1d48b8d907a8524218ed691503
JaxCavalera <cxstudios@hotmail.com>
2015-11-21 09:23:40 +1000

    Validation Complete 100%

--- app/views/scripts/auth-validation.jsx
+++ app/views/scripts/auth-validation.jsx
@@ -1,40 +1,40 @@
 export function unameValidation(text) {
     let usertxt = text;
-    let errorTxt = [];
+    let errorTxt = '';
 
     //  Check if unameInput is empty
     if (usertxt.length === 0) {
-        errorTxt.push('Username Is Missing');
+        errorTxt = 'Username Is Missing';
         return errorTxt;
     }
 
     if (usertxt === 'Username') {
-        errorTxt.push('Create New Account or Login');
+        errorTxt = 'Create New Account or Login';
         return errorTxt;
     }
 
     if (/\s+/g.test(usertxt)) {
-        errorTxt.push('Username Has Spaces');
+        errorTxt = 'Username Has Spaces';
         return errorTxt;
     }
 
-    return errorTxt = [];
+    return errorTxt;
 };
 
 export function pwordValidation(text) {
     let usertxt = text;
-    let errorTxt = [];
+    let errorTxt = '';
 
     //  Check if unameInput is empty
     if (usertxt.length === 0) {
-        errorTxt.push('Password Is Missing');
+        errorTxt = 'Password Is Missing';
         return errorTxt;
     }
 
     if (/\s+/g.test(usertxt)) {
-        errorTxt.push('Password Has Spaces');
+        errorTxt = 'Password Has Spaces';
         return errorTxt;
     }
 
-    return errorTxt = [];
+    return errorTxt;
 };
