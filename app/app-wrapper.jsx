3d855876af146c8fba93ff9db9520e40113a0170
JaxCavalera <cxstudios@hotmail.com>
2015-11-26 16:23:29 +1000

    Initial Routing is implemented correctly now

--- app/app-wrapper.jsx
+++ app/app-wrapper.jsx
@@ -1,16 +1,57 @@
-import React, {Component, PropTypes} from 'react';
+//  Redux and React Modules
+import {bindActionCreators} from 'redux';
+import React, {Component} from 'react';
+
+//  connect() gives the component access to the state tree (a.k.a store)
+import {connect} from 'react-redux';
+
 import Helmet from 'react-helmet'; // usage = <Helmet props=""/>
+
+//  =====  Import View Component Options  =====
+import HeadingView from './views/heading-view.jsx';
+import HeadingViewAuth from './views/heading-view-auth.jsx';
 
 //  =====  Import Helmet Assets (images data etc)  =====
 import AndroidIcon from './components/heading/images/androidicon.png';
 import MyManifest from './components/heading/images/manifest.json';
 
-export default class AppWrapper extends Component {
+/*
+properties declared in here become accessible
+in code located below inside the render() flux-standard-action
+The connection between this and the component is done at the bottom
+using the "connect()" function
+*/
+
+//  =====  "state" is being passed in from the top level <Provider> wrapper  =====
+function mapStateToProps(state) {
+    return {
+        heading: state.heading,
+    };
+}
+
+class AppWrapper extends Component {
     render() {
-        console.log(this.props.children);
+
+        //  =====  Define loginCheck Function  =====
+        const loginStatus = this.props.heading.status;
+
+        const loginStatusFunction = () => {
+            if (loginStatus === '' || loginStatus === 'fail') {
+
+                //  User is not logged in or login attempt failed
+                return <HeadingView />;
+            } else {
+
+                //  User successfully logged in
+                return <HeadingViewAuth />;
+            };
+        };
+
         return (
             <div className='appWrapper'>
+                {loginStatusFunction()}
                 {this.props.children}
+
                 {/* Helmet is used here to define Elements in the <head></head> */}
                 <Helmet
                     link={[
@@ -26,7 +67,4 @@ export default class AppWrapper extends Component {
     }
 }
 
-//  declare PropTypes here to lock variables into a specific type
-AppWrapper.propTypes = {
-
-};
+export default connect(mapStateToProps)(AppWrapper);
--- app/routes.jsx
+++ app/routes.jsx
@@ -6,19 +6,12 @@ import AppWrapper from './app-wrapper.jsx';
 import HomeView from './views/home-view.jsx';
 import GalleryView from './views/gallery-view.jsx';
 import EditorView from './views/editor-view.jsx';
-import HeadingView from './views/heading-view.jsx';
-import HeadingViewAuth from './views/heading-view-auth.jsx';
 
 const routes = (
     <Route component={AppWrapper}>
-        <Route component={HeadingView}>
-            <Route path='/' component={HomeView}></Route>
-        </Route>
-        <Route path='private' component={HeadingViewAuth}>
-            <Route path='private/home'component={HomeView}></Route>
-            <Route path='private/gallery' component={GalleryView}></Route>
-            <Route path='private/editor' component={EditorView}></Route>
-        </Route>
+        <Route path='/' component={HomeView}></Route>
+        <Route path='private/gallery' component={GalleryView}></Route>
+        <Route path='private/editor' component={EditorView}></Route>
     </Route>
 );
 
