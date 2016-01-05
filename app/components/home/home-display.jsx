e05a109d112d7ae43f7cb95b7e802e51a67c4693
JaxCavalera <cxstudios@hotmail.com>
2016-01-05 10:04:28 +1000

    Updated ES-Lint & Included constvars file
    constvars.jsx is a manual implementation of dotenv or similar.  dotenv
    is demonstrated inside the Tagged-Server and Tagged-Isomorphic repos.

--- app/components/home/home-display.jsx
+++ app/components/home/home-display.jsx
@@ -1,12 +1,12 @@
-import React, {Component, PropTypes} from 'react';
+import React, {Component} from 'react';
 import './home.css';
 
 export default class HomeDisplay extends Component {
     render() {
         return (
             <div className='homeBodyDisplay'>
-                <h1>Welcome to Tagged</h1>
-                <p>Start uploading your images and tagging other users today!</p>
+                <h1>{'Welcome to Tagged'}</h1>
+                <p>{'Start uploading your images and tagging other users today!'}</p>
             </div>
         );
     }
---
+++ app/constvars.jsx
@@ -0,0 +1,1 @@
+export const DBSERVER_VAR = 'http://cxstudios.duckdns.org:3000';
